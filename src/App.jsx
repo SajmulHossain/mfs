2d5cb40f673468ca4355891e0ee2edc394e6ecfa
Sajmul <sajmul1427@gmail.com>
2025-02-26 01:13:52 +0600

    admin panel design almost done

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,25 @@ import AgentLayout from "./layout/AgentLayout";
 import AdminLayout from "./layout/AdminLayout";
 import useAuth from "./hooks/useAuth";
 import Loading from "./components/Loading";
+import { initFlowbite } from "flowbite";
+import { useEffect } from "react";
 
 
 const App = () => {
  const { role, loading } = useAuth();
+ useEffect(() => {
+  initFlowbite();
+ },[])
 
  if(loading) {
   return <Loading />
  }
-console.log(role);
+ 
   if(role === 'user') {
     return <UserLayout />
   } else if(role === 'agent') {
     return <AgentLayout />
   } else if(role === 'admin') {
-    console.log('admin paice');
     return <AdminLayout />
   }
 
