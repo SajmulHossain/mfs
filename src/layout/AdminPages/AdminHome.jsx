5bb2521be545b92ab833c85a278d906744c5c55a
Sajmul <sajmul1427@gmail.com>
2025-02-26 22:50:37 +0600

    admin states added

--- src/layout/AdminPages/AdminHome.jsx
+++ src/layout/AdminPages/AdminHome.jsx
@@ -6,23 +6,78 @@ import {
 } from "react-icons/md";
 import { BsCashCoin } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { TbCoinTaka } from "react-icons/tb";
 
 const AdminHome = () => {
-  return (
-    <div className="flex justify-between flex-wrap">
-      <Link to='/users' className="btn justify-self-center flex-col gap-0 transition-all duration-200 hover:bg-main/20 p-4 rounded-md">
-        <MdOutlineManageAccounts size={60} />
-        <span className="font-semibold text-xs">Manage Users</span>
-      </Link>
+  const axiosSecure = useAxiosSecure();
+  const {data:states={}, isLoading} = useQuery({
+    queryKey: ['states'],
+    queryFn: async() => {
+      const {data} = await axiosSecure('/states');
+      return data || {};
+    }
+  })
 
-      <Link to='/agent-requests' className="btn justify-self-center flex-col transition-all duration-200 gap-0 hover:bg-main/20 p-4 rounded-md">
-        <MdOutlineRealEstateAgent size={60} />
-        <span className="font-semibold text-xs">Agent Request</span>
-      </Link>
-      <button className="btn justify-self-center flex-col transition-all duration-200 gap-0 hover:bg-main/20 p-4 rounded-md">
-        <BsCashCoin size={60} />
-        <span className="font-semibold text-xs">Cash In Request</span>
-      </button>
+  console.log(states);
+  return (
+    <div>
+      <div
+        className={`border border-main p-2 rounded-md ${
+          isLoading ? "animate-pulse gap-1" : "gap-2"
+        } flex justify-between`}
+      >
+        {isLoading ? (
+          <>
+            <div className="bg-main w-full rounded-md"></div>
+            <div className="h-16 w-[1px] bg-main"></div>
+            <div className="bg-main w-full rounded-md"></div>
+          </>
+        ) : (
+          <>
+            <div className="w-full">
+              <p className="font-semibold">Total Income</p>
+              <p className="mt-1 flex items-center gap-1">
+                <TbCoinTaka /> {states?.income.toFixed(2)}
+              </p>
+            </div>
+
+            <div className="w-[1px] bg-main"></div>
+            <div className="w-full">
+              <p className="font-semibold">
+                Total Money{" "}
+                <span className="text-xs font-normal">in system</span>
+              </p>
+              <p className="mt-1 flex items-center gap-1">
+                <TbCoinTaka />
+                {states?.totalMoney.toFixed(2)}
+              </p>
+            </div>
+          </>
+        )}
+      </div>
+      <div className="flex justify-between flex-wrap">
+        <Link
+          to="/users"
+          className="btn justify-self-center flex-col gap-0 transition-all duration-200 hover:bg-main/20 p-4 rounded-md"
+        >
+          <MdOutlineManageAccounts size={60} />
+          <span className="font-semibold text-xs">Manage Users</span>
+        </Link>
+
+        <Link
+          to="/agent-requests"
+          className="btn justify-self-center flex-col transition-all duration-200 gap-0 hover:bg-main/20 p-4 rounded-md"
+        >
+          <MdOutlineRealEstateAgent size={60} />
+          <span className="font-semibold text-xs">Agent Request</span>
+        </Link>
+        <button className="btn justify-self-center flex-col transition-all duration-200 gap-0 hover:bg-main/20 p-4 rounded-md">
+          <BsCashCoin size={60} />
+          <span className="font-semibold text-xs">Cash In Request</span>
+        </button>
+      </div>
     </div>
   );
 };
